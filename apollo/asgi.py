19101328ee424cedd25d5cc09d3c50318aaca57d
brunosag <brunosag02@gmail.com>
2022-08-04 08:40:48 -0300

    change project name to "apollo"

--- apollo/asgi.py
+++ apollo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for finalproject project.
+ASGI config for apollo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apollo.settings')
 
 application = get_asgi_application()
