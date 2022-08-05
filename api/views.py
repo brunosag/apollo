2e64703e738f481c20254f38be942647a5c29651
brunosag <brunosag02@gmail.com>
2022-08-05 14:52:01 -0300

    make registration view a generic CreateAPIView

--- api/views.py
+++ api/views.py
@@ -1,8 +1,6 @@
-from rest_framework import status
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from .serializers import RegisterSerializer
 
@@ -23,11 +21,5 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-class RegisterView(APIView):
-
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
