c926eb3f17ac829b3485c05eb92a7c965395639d
brunosag <brunosag02@gmail.com>
2022-09-19 17:20:35 -0300

    add nested objects to serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
 from .models import Board, List, Card
 
 
@@ -19,15 +18,16 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-
-    confirmation = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    confirmation = serializers.CharField(
+        style={'input_type': 'password'}, write_only=True
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'confirmation']
         extra_kwargs = {
             'email': {'required': True},
-            'password': {'validators': [validate_password]}
+            'password': {'validators': [validate_password]},
         }
 
     def validate(self, data):
@@ -37,29 +37,31 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email']
+            username=validated_data['username'], email=validated_data['email']
         )
         user.set_password(validated_data['password'])
         user.save()
         return user
 
 
-class BoardSeriazlier(serializers.ModelSerializer):
-    class Meta:
-        model = Board
-        fields = ['id', 'title', 'last_access', 'lists']
-        read_only_fields = ['last_access', 'lists']
-
-
-class ListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = List
-        fields = ['id', 'board', 'title', 'order', 'cards']
-        read_only_fields = ['cards']
-
-
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ['id', 'list', 'title', 'order']
+
+
+class ListSerializer(serializers.ModelSerializer):
+    cards = CardSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = List
+        fields = ['id', 'board', 'title', 'order', 'cards']
+
+
+class BoardSeriazlier(serializers.ModelSerializer):
+    lists = ListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Board
+        fields = ['id', 'title', 'last_access', 'lists']
+        read_only_fields = ['last_access']
